9190832805dbd624918cd47922a17be6c619f34f
Xiangyi Pan <lucas.pan@mail.utoronto.ca>
2016-11-25 14:40:26 -0500

    Development database

--- models/user.js
+++ models/user.js
@@ -37,8 +37,8 @@ var userSchema = new Schema(
 );
 
 // Doc for Mongoose Connections: http://mongoosejs.com/docs/connections
-// mongoose.connect('mongodb://localhost/usersdb');
-mongoose.connect('mongodb://ds021650.mlab.com:21650/heroku_7tg73962/usersdb');
+ mongoose.connect('mongodb://localhost/usersdb');
+// mongoose.connect('mongodb://ds021650.mlab.com:21650/heroku_7tg73962/usersdb');
 
 // Doc for Mongoose Models: http://mongoosejs.com/docs/models
 module.exports = mongoose.model('User', userSchema);
